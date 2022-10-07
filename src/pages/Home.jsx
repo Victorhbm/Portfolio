5c51d4e9bea7990fb7cc285358a6548d7a1485dd
Victorhbm <victor.hugofla@hotmail.com>
2022-10-07 16:34:35 +0100

    feat: create Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+import SectionAbout from "../components/SectionAbout";
+
+export default function Home() {
+  return (
+    <div className="font-inter">
+      <Header />
+      <SectionAbout />
+    </div>
+  )
+}
