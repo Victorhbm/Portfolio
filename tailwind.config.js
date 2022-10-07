622fde0280c8eb7703471bc8ba655fadd879717d
Victorhbm <victor.hugofla@hotmail.com>
2022-10-07 16:30:48 +0100

    chore: add keyframes and animations to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        rotate: {
+          to: { transform: 'rotate(-180deg)' }
+        },
+        smooth: {
+          from: { opacity: 0, transform: 'translateY(-30px)' },
+          to: { opacity: 1, transform: 'translateY(0px)' },
+        }
+      },
+      animation: {
+        rotate: 'rotate .3s ease',
+        smooth: 'smooth .3s ease-in-out'
+      }
+    },
     colors: {
       'dark': '#121212',
       'dark-200': '#151515',
