a83d7f29e27d4f85533cd63d2ce8a07f2ba56334
Victorhbm <victor.hugofla@hotmail.com>
2022-10-23 17:50:35 +0100

    feat: add ProjectsPage to route /projects

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Route, Switch } from "react-router-dom";
 import Home from "./pages/Home";
+import ProjectsPage from "./pages/ProjectsPage";
 
 function App() {
   return (
     <Switch>
+      <Route exact path="/projects" component={ProjectsPage} />
       <Route path="/" component={Home} />
     </Switch>
   );
