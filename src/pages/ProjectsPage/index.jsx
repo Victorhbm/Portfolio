005a92ac16e53c8cbcfdf3bcffa53c44154db122
Victorhbm <victor.hugofla@hotmail.com>
2022-10-23 17:47:30 +0100

    feat: create Projects page

---
+++ src/pages/ProjectsPage/index.jsx
@@ -0,0 +1,11 @@
+import Header from "../../components/Header";
+import Projects from "../../components/Projects";
+
+export default function ProjectsPage() {
+  return (
+    <div>
+      <Header />
+      <Projects />
+    </div>
+  );
+}
