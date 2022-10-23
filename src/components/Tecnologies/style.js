7745963a14ee3502748dfc748f8b0c78e85a90b6
Victorhbm <victor.hugofla@hotmail.com>
2022-10-23 17:00:40 +0100

    style(Tecnologies): add more styled components and responsive style

--- src/components/Tecnologies/style.js
+++ src/components/Tecnologies/style.js
@@ -1,39 +1,60 @@
 import tw from "tailwind-styled-components"
 
+export const Section = tw.section`
+  bg-dark-200
+  text-light
+`
+
 export const Container = tw.div`
   container
   mx-auto
   max-w-[1200px]
   px-5
   py-7
+
+  text-lg
+  md:text-xl
+  md:py-14
 `
 
 export const SectionTitle = tw.h2`
-  text-2xl
+  text-3xl
   uppercase
-  mb-5
+  mb-7
   font-semibold
 `
 
+export const TitleSpan = tw.span`
+  text-blue
+`
+
 export const SectionSubtitle = tw.h3`
-  text-xl
-  mt-5
+  text-2xl
+  mt-6
   mb-3
 `
 
 export const TecnologiesContainer = tw.div`
-  flex
-  flex-wrap
+  grid
+  grid-cols-2
+  gap-4
+
+  md:grid-cols-3
+  lg:grid-cols-4
 `
 
 export const TecnologiesContent = tw.div`
   flex
-  grow
+  flex-col
+  justify-center
   rounded-md
   items-center
+  p-4
+  gap-x-4
+  gap-y-2
+
+  md:flex-row
+
   text-gray-light
-  mr-3
-  mb-3
-  p-3
   bg-dark
 `
