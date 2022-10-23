1000b02d422bdc9a49a6c8771de8069f8355a446
Victorhbm <victor.hugofla@hotmail.com>
2022-10-23 16:58:14 +0100

    feat: create skills data

---
+++ src/assets/data/skills.js
@@ -0,0 +1,23 @@
+const skills = {
+  frontend: [
+    'JavaScript',
+    'TypeScript',
+    'React',
+    'Tailwind CSS',
+    'styled-components'
+  ],
+  backend: ['Node', 'MySQL', 'MongoDB', 'Sequelize'],
+  others: [
+    'Python',
+    'Git & GitHub',
+    'REST API',
+    'OOP',
+    'SOLID',
+    'Docker',
+    'VS Code',
+    'Jest',
+    'Mocha, Chai & Sinon'
+  ],
+}
+
+export default skills;
