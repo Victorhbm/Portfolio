0691462d2711c34643ff51edcf6a7752a36b8e8a
Victorhbm <victor.hugofla@hotmail.com>
2022-10-24 11:03:18 +0100

    style(ProjectsPage): add styled component

--- src/pages/ProjectsPage/index.jsx
+++ src/pages/ProjectsPage/index.jsx
@@ -1,11 +1,12 @@
 import Header from "../../components/Header";
 import Projects from "../../components/Projects";
+import { Container } from "./style";
 
 export default function ProjectsPage() {
   return (
-    <div>
+    <Container>
       <Header />
       <Projects />
-    </div>
+    </Container>
   );
 }
