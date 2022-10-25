4dc300feee300bd374e2d848e4b8a6e42e654c97
Victorhbm <victor.hugofla@hotmail.com>
2022-10-25 14:17:50 +0100

    feat: add Footer to ContactPage

--- src/pages/ContactPage/index.jsx
+++ src/pages/ContactPage/index.jsx
@@ -1,12 +1,16 @@
 import Header from "../../components/Header";
 import Contact from "../../components/Contact";
 import { Container } from "./style";
+import Footer from "../../components/Footer";
 
 export default function ContactPage() {
   return (
     <Container>
-      <Header />
-      <Contact bgDark={true} />
+      <div>
+        <Header />
+        <Contact bgDark={true} />
+      </div>
+      <Footer />
     </Container>
   );
 }
