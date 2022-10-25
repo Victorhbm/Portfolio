b917991c12c62be2e50e107c834425204ff474a6
Victorhbm <victor.hugofla@hotmail.com>
2022-10-25 14:16:37 +0100

    feat: create Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import { Container, StyledFooter } from "./style";
+
+export default function Footer() {
+  return (
+    <StyledFooter>
+      <Container>
+        <p>&copy; Victor Hugo - 2022</p>
+      </Container>
+    </StyledFooter>
+  )
+}
