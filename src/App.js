3fbee8ccf091a4c58b958faa30cdf64348f63ae5
Victorhbm <victor.hugofla@hotmail.com>
2022-10-25 14:21:16 +0100

    feat: add ThanksPage to route /thanks

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Switch } from "react-router-dom";
 import ContactPage from "./pages/ContactPage";
 import Home from "./pages/Home";
 import ProjectsPage from "./pages/ProjectsPage";
+import ThanksPage from "./pages/ThanksPage";
 
 function App() {
   return (
     <Switch>
       <Route exact path="/projects" component={ProjectsPage} />
       <Route exact path="/contact" component={ContactPage} />
+      <Route exact path="/thanks" component={ThanksPage} />
       <Route path="/" component={Home} />
     </Switch>
   );
