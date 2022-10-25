87d9bb56026a7ee43919d4428207e33d505ada3a
Victorhbm <victor.hugofla@hotmail.com>
2022-10-25 14:16:08 +0100

    style(Footer): add styled components

---
+++ src/components/Footer/style.js
@@ -0,0 +1,18 @@
+import tw from "tailwind-styled-components";
+
+export const Container = tw.div`
+  container
+  mx-auto
+  max-w-[1200px]
+  px-5
+  py-4
+
+  sm:px-10
+  sm:py-7
+  lg:xl:px-5
+`
+
+export const StyledFooter = tw.footer`
+  bg-dark
+  text-light
+`
